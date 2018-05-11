ff08181670527993e6f3b3e1a6db7dbd16a1496b
huhuiyu <1069306849@qq.com>
2018-05-11 15:27:55 +0800

    多余的导入

--- myspringboot/src/main/java/top/huhuiyu/myspringboot/config/MyExceptionHandler.java
+++ myspringboot/src/main/java/top/huhuiyu/myspringboot/config/MyExceptionHandler.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import top.huhuiyu.myspringboot.util.JsonMessage;
